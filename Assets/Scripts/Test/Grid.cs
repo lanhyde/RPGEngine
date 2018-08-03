2e2d31bc5d6b3cb55046b75a2524dc671fbc8ebf
XinRemilia <woshinixinzong@qq.com>
2018-08-03 21:47:53 +0800

    Test 20180803

---
+++ Assets/Scripts/Test/Grid.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class Grid : MonoBehaviour
+{
+    public bool m_switch;//スイッチ
+    Image m_image;
+    private Canvas m_canvas;
+    void Start()
+    {
+
+        m_switch = true;
+        m_image = GetComponent<Image>();
+        m_canvas = GameObject.Find("Canvas").GetComponent<Canvas>();
+
+        UIEventListener.Get(gameObject).onPointEnter = PointEnter;
+        UIEventListener.Get(gameObject).onPointExit = PointExit;
+        UIEventListener.Get(gameObject).onBeginDrag = PointBeginDrag;
+
+    }
+
+    //Enter
+    public void PointEnter(GameObject go)
+    {
+        if (m_switch)
+            m_image.color = Color.green;
+    }
+    //Exit
+    public void PointExit(GameObject go)
+    {
+        if (m_switch)
+            m_image.color = Color.white;
+    }
+    //Drag
+    public void PointBeginDrag(GameObject go)
+    {
+        Instantiate<GameObject>(gameObject);
+       
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    //public void vvv()
+    //{
+    //    Vector2 postion;
+    //    RectTransformUtility.ScreenPointToLocalPointInRectangle(m_canvas.transform as RectTransform,Input.mousePosition,null,out postion);
+
+
+    //}
+}
---
+++ Assets/Scripts/Test/Test.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Test : MonoBehaviour {
+
+    public GameObject m_grid;
+
+    public RectTransform m_map; //base map
+
+    public List<GameObject> m_grids;
+    void Start ()
+    {
+        m_map = GetComponent<RectTransform>();
+        int mapWidth = (int)m_map.sizeDelta.x;
+        int mapHeight = (int)m_map.sizeDelta.y;
+
+        RectTransform grid = m_grid.GetComponent<RectTransform>();
+        int gridWidth = (int)grid.sizeDelta.x;
+        int gridHeight = (int)grid.sizeDelta.y;
+
+
+        for (int i = 0; i < (mapWidth / gridWidth) * (mapHeight / gridHeight); i++)//print map
+        {
+            GameObject temp = Instantiate<GameObject>(m_grid);
+            temp.name = "Grid" + i;
+            temp.transform.SetParent(transform.Find("Group"));
+            temp.AddComponent<Grid>();
+            m_grids.Add(temp);
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
